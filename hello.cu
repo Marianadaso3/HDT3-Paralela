5106f57f5c6ca8738ca36a6539062622cb54500c
Marianadaso3 <72479030+Marianadaso3@users.noreply.github.com>
2023-11-10 16:43:37 -0600

    first implementation

---
+++ hello.cu
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+__global__ void helloCUDA()
+{
+    int threadID = threadIdx.x; // Modifica la forma en que se calcula el identificador del hilo
+    printf("Hello, World! I'm thread %d. Nuestro nombres son Mariana David 201055, Angel Higueros 20460 y Pablo Escobar 20963\n", threadID);
+    
+    // if (threadID == 0) {
+    //     printf("Kernel ejecutado correctamente\n");
+    // } else {
+    //     printf("Error en el kernel: threadID != 0\n");
+    // }
+}
+
+int main()
+{
+    int numBlocks = 1;          // Número de bloques (1 bloque)
+    int threadsPerBlock = 1024; // Número de hilos por bloque (2048 hilos)
+
+    // Llama al kernel con la configuración de bloques y hilos
+    helloCUDA<<<numBlocks, threadsPerBlock>>>();
+    helloCUDA<<<numBlocks, threadsPerBlock>>>();
+
+    // Espera a que todos los hilos hayan terminado
+    cudaDeviceSynchronize();
+
+    //printf("Fin del programa\n");
+
+    return 0;
+}
