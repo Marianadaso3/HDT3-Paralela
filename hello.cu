2191b38f0463ab5ef2bad7a1b2776b6f294d8e02
Marianadaso3 <72479030+Marianadaso3@users.noreply.github.com>
2023-11-10 16:45:12 -0600

    corrections

--- hello.cu
+++ hello.cu
@@ -3,7 +3,7 @@
 __global__ void helloCUDA()
 {
     int threadID = threadIdx.x; // Modifica la forma en que se calcula el identificador del hilo
-    printf("Hello, World! I'm thread %d. Nuestro nombres son Mariana David 201055, Angel Higueros 20460 y Pablo Escobar 20963\n", threadID);
+    printf("Hello, World! I'm thread %d. Nuestro nombres son Mariana David 201055, Angel Higueros 20460 y Pablo Escobar 20936\n", threadID);
     
     // if (threadID == 0) {
     //     printf("Kernel ejecutado correctamente\n");
